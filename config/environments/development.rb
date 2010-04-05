e7ca8870d74e0792fba97ce1246a7b4d613f24c7
Mario Alberto Chavez <mario.chavez@gmail.com>
2010-04-04 20:16:52 -0700

    Finalmente se configuro una cuenta de decisionesinteligentes.com para usar como smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
-
+#require 'smtp-tls'  
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
@@ -14,4 +14,7 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default_charset = "utf-8"
 config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,10 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
- config.action_mailer.raise_delivery_errors = false
- config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default_charset = "utf-8"
+config.action_mailer.raise_delivery_errors = true
  
 # Enable threaded mode
 # config.threadsafe!
